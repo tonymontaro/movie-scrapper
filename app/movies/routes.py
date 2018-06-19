e5c98f39829bbfbfa4da7f81ca0e4bac4a08e0d6
Anthony Ngene <ngeneanthony@gmail.com>
2018-06-19 17:08:12 +0100

    implement sorting movies by name and time

--- app/movies/routes.py
+++ app/movies/routes.py
@@ -2,7 +2,7 @@
 from flask import Blueprint, jsonify, request
 
 from app.models import Movie
-from app.movies.helper import scrape_movies
+from app.movies.helper import scrape_movies, get_earliest_movie_time
 
 movies_bp = Blueprint('api', __name__)
 
@@ -22,8 +22,16 @@ def home():
     search = request.args.get('q')
     if search:
         movies = Movie.find(search)
+    movies = [movie.get_content() for movie in movies]
 
-    return jsonify([movie.get_content() for movie in movies]), 200
+    sort = request.args.get('sort')
+    if sort and sort.lower() == 'name':
+        movies = sorted(movies, key=lambda movie: movie['name'])
+    elif sort and sort.lower() == 'time':
+        movies = sorted(
+            movies, key=lambda movie: get_earliest_movie_time(movie))
+
+    return jsonify(movies), 200
 
 
 @movies_bp.route('/<int:id_>', methods=['GET'])
